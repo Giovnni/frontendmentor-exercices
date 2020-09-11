0b903fce912535691343e982980d3189be98405b
Giovanni Haddadi <giovanni.haddadi@isen.yncrea.fr>
2020-09-12 01:59:20 +0200

    javascript update on picture, need to correct the appearance of the second picture

--- coding-bootcamp-testimonials-slider/script.js
+++ coding-bootcamp-testimonials-slider/script.js
@@ -8,59 +8,84 @@ clickLeft.addEventListener("click", slideLeft);
 
 function slideRight() {
     var testimonial = document.querySelectorAll(".testimonial-infos");
+    var image = document.querySelectorAll(".client-picture");
     if (i == (testimonial.length - 1)) {
         testimonial[i].classList.remove('leftOut')
         testimonial[i].classList.remove('leftIn')
+        image[i].classList.remove('rightOut');
+        image[i].classList.remove('rightIn');
         if (testimonial[i].classList.contains("rightIn")) {
             testimonial[i].classList.replace("rightIn", "rightOut");
+            image[i].classList.replace("leftIn", "leftOut");
             i = 0;
             testimonial[i].classList.remove('leftOut')
             testimonial[i].classList.remove('leftIn')
+            image[i].classList.remove('rightOut');
+            image[i].classList.remove('rightIn');
             if (testimonial[i].classList.contains("rightOut")) {
                 testimonial[i].classList.replace("rightOut", "rightIn");
+                image[i].classList.replace("leftOut", "leftIn");
             }
             else {
                 testimonial[i].classList.add('rightIn');
+                image[i].classList.add("leftIn");
             }
         }
         else {
             testimonial[i].classList.add('rightOut');
+            image[i].classList.add("leftOut");
             i = 0;
             testimonial[i].classList.remove('leftOut')
             testimonial[i].classList.remove('leftIn')
+            image[i].classList.remove('rightOut');
+            image[i].classList.remove('rightIn');
             if (testimonial[i].classList.contains("rightOut")) {
                 testimonial[i].classList.replace("rightOut", "rightIn");
+                image[i].classList.replace("leftOut", "leftIn");
             }
             else {
                 testimonial[i].classList.add('rightIn');
+                image[i].classList.add("leftIn");
             }
         }
     }
     else {
         testimonial[i].classList.remove('leftOut')
         testimonial[i].classList.remove('leftIn')
+        image[i].classList.remove('rightOut');
+        image[i].classList.remove('rightIn');
         if (testimonial[i].classList.contains("rightIn")) {
             testimonial[i].classList.replace("rightIn", "rightOut");
+            image[i].classList.replace("leftIn", "leftOut");
             i++;
             testimonial[i].classList.remove('leftOut')
             testimonial[i].classList.remove('leftIn')
+            image[i].classList.remove('rightOut');
+            image[i].classList.remove('rightIn');
             if (testimonial[i].classList.contains("rightOut")) {
                 testimonial[i].classList.replace("rightOut", "rightIn");
+                image[i].classList.replace("leftOut", "leftIn");
             }
             else {
                 testimonial[i].classList.add('rightIn');
+                image[i].classList.add("leftIn");
             }
         }
         else {
             testimonial[i].classList.add('rightOut');
+            image[i].classList.add("leftOut");
             i++;
             testimonial[i].classList.remove('leftOut')
             testimonial[i].classList.remove('leftIn')
+            image[i].classList.remove('rightOut');
+            image[i].classList.remove('rightIn');
             if (testimonial[i].classList.contains("rightOut")) {
                 testimonial[i].classList.replace("rightOut", "rightIn");
+                image[i].classList.replace("leftOut", "leftIn");
             }
             else {
                 testimonial[i].classList.add('rightIn');
+                image[i].classList.add("leftIn");
             }
         }
     }
